f96dfd1bf0debcebf7a6c239af5972ca004dd98d
tnphat203 <tnphat203@gmail.com>
2026-07-27 14:16:10 +0700

    update refresh token

--- be/src/controllers/auth.controller.js
+++ be/src/controllers/auth.controller.js
@@ -96,9 +96,10 @@ const login = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: "/",
     });
 
     res.json({
